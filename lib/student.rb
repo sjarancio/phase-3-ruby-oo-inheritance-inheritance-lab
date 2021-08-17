54b20f55e33dae51e12aab7766768a71ccf7eac8
Stephen Arancio <sjarancio@gmail.com>
2021-08-17 17:04:28 -0400

    commit tree 72c050f2f62f2f851355b9dd10f27b31374e58c2

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        @knowledge=[]
+    end
+    def learn(str)
+        @knowledge << str
+    end
+    def knowledge 
+        @knowledge
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+
+    def last_name=(last_name)
+        @last_name = last_name
+    end
+    def last_name
+        @last_name
+    end
+
+    def first_name=(first_name)
+        @first_name = first_name
+    end
+    def first_name
+        @first_name
+    end
+end
